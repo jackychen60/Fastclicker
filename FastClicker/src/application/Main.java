8f1ec707dc8ec6aed4bc04f644448283db8f715d
jacky <jacky@DESKTOP-PTU6VTF>
2018-04-13 00:51:04 -0400

    woo

--- FastClicker/src/application/Main.java
+++ FastClicker/src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -11,53 +12,69 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
  
 public class Main extends Application {
+	private int score = 0;
+	private boolean scoring = false;
+	private long timeStep;
+	
     public static void main(String[] args) {
         launch(args);
     }
-    private boolean scoring;
-	private int score;
+   
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Fastclicker");
         Button btn = new Button();
         btn.setText("Click.");
-        Text txt = new Text(10,0,"Click score");
+        Text txt = new Text(10,0,"Click.");
         btn.setOnAction(new EventHandler<ActionEvent>() {
- 
           
-
-			@Override
-            public void handle(ActionEvent event) {
-                if(scoring)
-                {
-                	score++;
-                }
+            @Override
+            public void handle(ActionEvent event) 
+            {
+            	
+               if(scoring) 
+               {
+            	   score++;
+            	   }
+               else 
+               {
+            	  score--;
+               }
             }
         });
-        
+         timeStep = System.nanoTime() +1000000000L;
         new AnimationTimer()
         {
-        	long timeStep = System.nanoTime() +1000000000L;
-        	public void handle(long now)
+        	 public void handle(long now)
         	{
-        		if(now<timeStep) {
-        			btn.setText("Click");
+        		if(now > timeStep)
+        		{
+        			timeStep = now + 10000000000L;
+        			scoring = !scoring;
+        		}
+        		if(!scoring)
+        		{
+        			btn.setText("Don't click!");   			
+        		}
+
+        		else
+        		{
+        			btn.setText("Click me");
         			
         		}
-        		if(now>timeStep)
-        	
-        		{
-        			txt.setText("Score:"+Integer.toString(score));
-        		}
+                 
+        		txt.setText("SCORE: " + Integer.toString(score));
         		
-        	}
+            }
         }.start();
    
         
         
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        root.getChildren().add(txt);
+        root.setAlignment(txt, Pos.TOP_LEFT);
+        primaryStage.setScene(new Scene(root, 400, 250));
         primaryStage.show();
     }
 }
